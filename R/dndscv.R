d12e89867220db0bdc89100a9605fb2fe5b7ff89
Declan Bennett <declanbennett@hotmail.com>
2020-07-13 12:07:53 +0100

    Update dndscv.R

--- R/dndscv.R
+++ R/dndscv.R
@@ -388,7 +388,7 @@ dndscv = function(mutations, gene_list = NULL, refdb = "hg19", sm = "192r_3w", k
             llall = sum(dpois(x=x$N, lambda=x$L*mutrates*mrfold*t(array(c(1,w),dim=c(4,numrates))), log=T)) # loglik free wmis, wnon, wspl
             w[w>1e4] = 1e4
             
-            p = 1-pchisq(2*(llall-c(llmis,ll0)),df=c(1,3))
+            p = 1-pchisq(2*(llall-c(llmis,ll0)),df=c(1,3),log.p=TRUE)
             return(c(w,p))
         }
         
@@ -488,7 +488,7 @@ dndscv = function(mutations, gene_list = NULL, refdb = "hg19", sm = "192r_3w", k
           
             if (constrain_wnon_wspl == 0) {
             
-                p = 1-pchisq(2*(llall_unc-c(llmis,lltrunc,ll0)),df=c(1,2,3))
+                p = 1-pchisq(2*(llall_unc-c(llmis,lltrunc,ll0)),df=c(1,2,3),log.p=TRUE)
                 return(c(wfree,p))
                 
             } else { # d2. Free selection model: free wmis, free wnon==wspl
@@ -496,7 +496,7 @@ dndscv = function(mutations, gene_list = NULL, refdb = "hg19", sm = "192r_3w", k
                 wmisfree = y[2]/y[6]/mrfold; wmisfree[y[2]==0] = 0
                 wtruncfree = sum(y[3:4])/sum(y[7:8])/mrfold; wtruncfree[sum(y[3:4])==0] = 0
                 llall = sum(dpois(x=x$N, lambda=x$L*mutrates*mrfold*t(array(c(1,wmisfree,wtruncfree,wtruncfree),dim=c(4,numrates))), log=T)) + dgamma(opt_t, shape=shape, scale=scale, log=T) # loglik free wmis, free wnon==wspl
-                p = 1-pchisq(2*c(llall_unc-llmis,llall-c(lltrunc,ll0)),df=c(1,1,2))
+                p = 1-pchisq(2*c(llall_unc-llmis,llall-c(lltrunc,ll0)),df=c(1,1,2),log.p=TRUE)
                 return(c(wmisfree,wtruncfree,wtruncfree,p))
             }
         }
